79bca088b6fe6a5e9d91160cc46bb5f09f465dc3
Ashish301101 <ashishappu14@gmail.com>
2021-01-26 14:49:55 +0530

    week-2

---
+++ keil lab/Week-2/str_cmp_week2.s
@@ -0,0 +1,14 @@
+	AREA strcpy,CODE,READWRITE 
+
+	  adr r0,source;
+	  adr r1,destination;
+	
+	  mov r2,#8;
+loop1 ldrb r3,[r0],#1;
+  	  strb r3,[r1],#1;
+	  subs r2,r2,#1;
+	  bne loop1
+
+source dbc 'p','a','v','i','t','h','r','a';
+destination dbc '0','0','0','0','0','0','0','0';
+end
